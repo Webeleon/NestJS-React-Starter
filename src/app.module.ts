04b1dbe313447fb5d8d2fc64d88ac4c461e45b61
Julien Prugne <julien@webeleon.dev>
2020-12-08 10:41:09 +0100

    setup mongoose close #11

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from './config/config.module';
+import {ConfigService} from './config/config.service';
+
+const configs = new ConfigService;
 
 @Module({
-  imports: [ConfigModule],
+  imports: [
+    ConfigModule,
+    MongooseModule.forRoot(configs.mongoUrl), 
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -5,9 +5,11 @@ import { config } from 'dotenv';
 @Injectable()
 export class ConfigService {
   public readonly port: number;
+  public readonly mongoUrl: string;
 
   constructor() {
     config();
     this.port = parseInt(process.env.PORT) || 5000;
+    this.mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/starter_webeleon'
   }
 }
